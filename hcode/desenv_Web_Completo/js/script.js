69a4ffa0f843acd57b36164839e1497c6800d9e2
Marcio Rela <marciorela75@gmail.com>
2020-10-22 16:55:17 -0300

    hcode:Completo - Aula 22

---
+++ hcode/desenv_Web_Completo/js/script.js
@@ -0,0 +1,17 @@
+function somar(a, b) {
+    return a + b;
+}
+
+document
+    .querySelector("#calcular")
+    .addEventListener("click", function() {
+        let valorA = document.querySelector("#valorA").value;
+        let valorB = document.querySelector("#valorB").value;
+
+        if (valorA.length > 0 && valorB.length > 0) {
+            // alert(parseInt(valorA) + parseInt(valorB));
+            alert(somar(parseInt(valorA), parseInt(valorB)));
+        } else {
+            alert("Digite os valores para o cálculo");
+        }
+});
